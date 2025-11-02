525feb86664cecbae4ecf35ae48c772994fba054
ReverseBed <55123649+Dimensionslash@users.noreply.github.com>
2025-11-02 01:23:19 -0400

    new

--- site/next.config.js
+++ site/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     name: 'Zenos Technologies',
     description: 'A unofficial registry for the domain site Zenos',
-    icon: 'https://raw.githubusercontent.com/Dimensionslash/Zenos-registry/f3ccffa9237e50a6bec4b4e19dfc89d193225a4a/site/img/logo.svg',
+    icon: 'https://raw.githubusercontent.com/Dimensionslash/Zenos-registry/1d053dc6e90ecbcaf1eb0be91c99833950663527/site/img/logo.svg',
     listUrl: 'https://dimensionslash.github.io/Zenos-registry/',
     contactUrl: 'https://github.com/Dimensionslash/Zenos-registry/issues',
   },
